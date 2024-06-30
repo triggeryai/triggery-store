07101a672a0482c1fb8d9a0b9d27a7dc289b5e93
unknown <office.travilabs@gmail.com>
2024-06-30 19:50:16 +0200

    modals cart also udpated

--- components/header/CartModal.tsx
+++ components/header/CartModal.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import Link from 'next/link';
 import useCartService from '@/lib/hooks/useCartStore';
+import Image from 'next/image';
 
 const CartModal = () => {
   const { items, itemsPrice, decrease, increase } = useCartService();
@@ -18,7 +19,7 @@ const CartModal = () => {
 
   // Ustawienie wysokości dla 7 elementów listy.
   // Jeśli jeden element listy ma więcej niż 50px, zmień wartość poniżej odpowiednio.
-  const maxHeightForSevenItems = 50 * 7;
+  const maxHeightForSevenItems = 70 * 7;
 
   return (
     <div className="absolute right-0 p-3 w-72 bg-white shadow-lg z-50" 
@@ -27,6 +28,15 @@ const CartModal = () => {
         {items.map((item, index) => (
           <li key={item.slug} className="flex justify-between items-center mb-2">
             <div className="flex items-center">
+              {item.mainImage && (
+                <Image
+                  src={item.mainImage}
+                  alt={item.name}
+                  width={50}
+                  height={50}
+                  className="object-cover"
+                />
+              )}
               <span className="ml-2">{item.name}</span>
             </div>
             <div className="flex items-center">
